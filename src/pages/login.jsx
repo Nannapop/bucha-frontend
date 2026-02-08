ebfa9dbb73cda7a2bb1ff801e33b3fe08f9f9b06
ORIGAMI <nannapop004@gmail.com>
2026-02-08 16:26:39 +0700

    fix login css case sensitivity

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Login.css";
+import "./login.css";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
